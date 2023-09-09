290059feefd88110abe6332424cb780bb4bd73c8
Marie Boussard <marie.boussard@student-cs.fr>
2023-09-09 14:11:16 +0200

    modified plots

--- sub2/plot.jl
+++ sub2/plot.jl
@@ -1,13 +1,30 @@
 using Plots
+using LaTeXStrings
 
 function plotCLD(cld::Vector)
-    histogram(cld,bins = range(0,6,100))
+    histogram(cld, bins=range(0, 6, 100))
     title!("Chord Length Distribution")
+    xlabel!("Chord Length")
+    ylabel!("Number of Simulated Measurements")
+
 end
 
 
-function plot2D(points::Vector, pl::Plan= XY())
-    npoints = length(points[1]) == 2 ? points : apply(p->projectTo(pl,p), points)
-    scatter(npoints)
+function plot2D(points::Vector, pl::Plan=XY())
+    npoints = length(points[1]) == 2 ? points : apply(p -> projectTo(pl, p), points)
+    scatter(npoints, aspect_ratio=:equal)
     title!("Projection ")
+    xlabel!(L"x")
+    ylabel!(L"y")
+end
+
+function plotConvexHull(points::Vector, convexHullPoints::Vector=convexHull(points))
+    @show points
+    @show convexHullPoints
+    #scatter(points, aspect_ratio=:equal, label="all", mc=:blue)
+    scatter(convexHullPoints, aspect_ratio=:equal, label="convex hull", mc=:red)
+    xlabel!(L"x")
+    ylabel!(L"y")
+    title!("Convex hull of the projection")
+
 end
--- sub2/script.jl
+++ sub2/script.jl
@@ -10,7 +10,10 @@ shape = cube(4)
 
 rot_shape = apply(p -> Rotation(π, π / 3, π / 4, p), shape)
 
-plot2D(rot_shape)
+#plot2D(rot_shape)
+pl = XY()
+npoints = length(rot_shape[1]) == 2 ? rot_shape : apply(p -> projectTo(pl, p), rot_shape)
+plotConvexHull(npoints)
 
 #=
 @time cld = computeCLD(shape, 10000; Φ = 0, ϕ = 0)
