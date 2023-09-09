91685bd793b680b4ace39de1b9436a8ff9c76d15
Marie Boussard <marie.boussard@student-cs.fr>
2023-09-09 19:00:13 +0200

    using test

--- sub2/test_cld.jl
+++ sub2/test_cld.jl
@@ -1,3 +1,4 @@
+using test
 include("cld.jl")
 
 function test_chordlength(points, h)
@@ -7,11 +8,13 @@ function test_chordlength(points, h)
     p_above_min = length(above_h) == 1 ? [_min(above_h, 2)] : two_min(above_h, 2, 1)
     p_below_max = length(below_h) == 1 ? [_max(below_h, 2)] : two_max(below_h, 2, 1)
 
-    x_left = intersect2D(p_above_min[1], p_below_max[1], h)
-    x_right = intersect2D(p_above_min[end], p_below_max[end], h)
+    @show x_left = intersect2D(p_above_min[1], p_below_max[1], h)
+    @show x_right = intersect2D(p_above_min[end], p_below_max[end], h)
     cl = abs(x_right - x_left)
 
+
     return cl, x_left, x_right
+
 end
 
 function test_computeCL(cp::ConvexPolyhedron, p::Plan=XY(); Φ::Real=2π * rand(), θ::Real=π * rand(), ϕ::Real=π / 2 * rand())
@@ -23,4 +26,5 @@ function test_computeCL(cp::ConvexPolyhedron, p::Plan=XY(); Φ::Real=2π * rand(
     yₗ = rand() * (edgeₘₐₓ - edgeₘᵢₙ) + edgeₘᵢₙ
     test_chordlength(ConvV, yₗ)
 end
+
 
