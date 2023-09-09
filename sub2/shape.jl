d911055135e476c82784f116acbaebfbc4e21de4
YvainLagelouze <144465622+YvainLagelouze@users.noreply.github.com>
2023-09-09 10:50:20 +0200

    Add tetraedron to shape.jl

--- sub2/shape.jl
+++ sub2/shape.jl
@@ -6,7 +6,13 @@ pave(a,b,c) = [(a/2,b/2,c/2),   (-a/2,-b/2,-c/2), (a/2,-b/2,-c/2), (-a/2,b/2,-c/
 
 cube(c) = pave(c,c,c)
 
-# tetrahedre
+function tetraedron(c) #c is sidelength
+    co = c / (2 * √2) #coord of the points
+    return [(co, co, co), (co, -co, -co), (-co, co, -co), (-co, -co, co)]
+    #each couple of points must have opposite values on exactly 2 axis
+    #so that the distance is indeed c
+end
+
 # pyramide
 
 
