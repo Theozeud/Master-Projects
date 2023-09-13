b49bf90fea795051db4b8e13b558c0ed36844ba2
Marie Boussard <marie.boussard@student-cs.fr>
2023-09-13 23:42:45 +0200

    improvements in opti and script_opti

--- sub2/scr/opti.jl
+++ sub2/scr/opti.jl
@@ -2,7 +2,7 @@ using Distances
 using Optim
 using LaTeXStrings
 
-function loss(K::Matrix, ψ::Vector, q::Vector, ε::Real, Ψₘ::Real=0.2)
+function loss(K::Matrix, ψ::Vector, q::Vector, ε::Real, Ψₘ::Real)
     @assert length(collect(axes(K)[2])) == length(ψ)
     @assert length(collect(axes(K)[1])) == length(q)
     sqeuclidean(K * ψ, q) + ε * norm(ψ .- Ψₘ)^2
@@ -11,13 +11,22 @@ end
 defaultInit(ψ::Vector) = zero(ψ)
 
 
-function optimizePSD(K::Matrix, q::Vector, ε::Real, method=LBFGS(), ψ₀=defaultInit(length(q)), loss=loss)
-    Optim.optimize(ψ -> loss(K, ψ, q, ε), ψ₀; iterations=100000)
+function optimizePSD(K::Matrix, q::Vector, ε::Real, Ψₘ::Real=0, method=LBFGS(), ψ₀=defaultInit(length(q)), loss=loss)
+    Optim.optimize(ψ -> loss(K, ψ, q, ε, Ψₘ), ψ₀; iterations=100000)
 end
 
-function plotOptiRes(rList, Ψₙᵤₘ, Ψₜᵣᵤₑ)
+function plotOptiRes(rList::Vector, Ψₙᵤₘ::Vector, Ψₜᵣᵤₑ::Vector)
     plot(rList, Ψₙᵤₘ, label="Numerical Resolution", linewidth=3, dpi=300)#, yticks=0:0.4)
     plot!(rList, Ψₜᵣᵤₑ, label="Exact", linewidth=3, dpi=300)
+    xlabel!("Particle Size")
+    ylabel!("Probability density")
+    title!("Particle Size Distribution")
+end
+
+function plotOptiRes(rList::Vector, Ψₙᵤₘ::Vector, Ψₜᵣᵤₑ::Vector, Ψ₀::Vector)
+    plot(rList, Ψₙᵤₘ, label="Numerical Resolution", linewidth=3, dpi=300)#, yticks=0:0.4)
+    plot!(rList, Ψₜᵣᵤₑ, label="Exact", linewidth=3, dpi=300)
+    plot!(rList, Ψ₀, label="Initial Distribution", linewidth=3, dpi=300)
     xlabel!("Particle Size")
     ylabel!("Probability density")
     title!("Particle Size Distribution")
@@ -27,6 +36,10 @@ function computeError(ψₙᵤₘ, ψₜᵣᵤₑ)
     sqeuclidean(ψₙᵤₘ, ψₜᵣᵤₑ)
 end
 
-function plotError(ψₙᵤₘ, ψₜᵣᵤₑ)
-
+function plotError(rList, ψₙᵤₘ, ψₜᵣᵤₑ)
+    error = [sqrt(((ψₙᵤₘ[i] - ψₜᵣᵤₑ[i]))^2) for i in 1:length(ψₙᵤₘ)]
+    plot(rList, error, linewidth=3, dpi=300, label=L"\Psi_{\text{num}} - \Psi_{\text{exact}}")
+    xlabel!("Particle Size")
+    ylabel!("Error")
+    title!("Error in the Numerical Resolution")
 end
--- sub2/scripts/script_opti.jl
+++ sub2/scripts/script_opti.jl
@@ -12,65 +12,49 @@ sizeL = 100
 ntir = 1000000
 ntirCLD = ntir
 
-# Random size
+# Choice of the Particle Sized Distribution
 #random = Uniform(5, 10)
 random = Normal(5, 0.5)
-#R = tir(random, sizeR)
-
-# Given L (comes from the mesurement)
-#L = range(1,4,sizeL) # Problème ??
 
 # Base Shape
 shape = Cube(1)
 
-# L comes is randomly chosen in the experimental cld obtained with the shape
+# Ltir is randomly chosen in the CLD obtained with the shape (measurement)
 Ltir = computeCLD(shape, ntir)
 
-# Utils function
-#bins, bins_number = computeCumulCLD(shape, ntir, 10000)
-
 # Computation of the matrix K
-rMax = 10
-#lMax = maximum(Ltir)
-lMax = rMax * maximum(Ltir)
+rMax = 10 # max particle size
+lMax = rMax * maximum(Ltir) # max chord length
 rList = collect(range(0, rMax, sizeR))
 lList = collect(range(0, lMax, sizeL))
 cumulCLD = computeCumulCLD(Ltir)
 K = matrixCLD(sizeL, sizeR, lList, rList, cumulCLD)
 
 # Computation of the CLD for different size
-function computeCLDdifferentSize()
-    cld = zeros(ntir)
-    # Random measurement
-    for i in 1:ntir
-        r = tir(random, 1)[1]
-        cp = Cube(r)
-        cld[i] = computeCL(cp)
-    end
-    bins, bins_number = computeCumulCLD(cld, 10000)
-    [probaCLD(l, cumulCLD) for l in range(0, lMax, sizeL)]
-end
-#q = computeCLDdifferentSize()
+#function computeCLDdifferentSize()
+#    cld = zeros(ntir)
+# Random measurement
+#    for i in 1:ntir
+#        r = tir(random, 1)[1]
+#        cp = Cube(r)
+#        cld[i] = computeCL(cp)
+#    end
+#    bins, bins_number = computeCumulCLD(cld, 10000)
+#    [probaCLD(l, cumulCLD) for l in range(0, lMax, sizeL)]
+#end
 
-# q = KΨ
-Ψ = apply(x -> law(random, x), rList)
+# Computation of CLD with q = KΨ
+Ψ = apply(x -> law(random, x), rList) # Exact distribution
 q = K * Ψ
 
 # Optimisation
-ψ₀ = [0.2 + rand() * 0.01 for i in 1:sizeR]
+ψ₀ = [0.2 + rand() * 0.01 for i in 1:sizeR] # Initial distribution
 
 method = LBFGS()
-ε = 1
+ε = 0.1 # Regularisation coefficient
 
 opt = optimizePSD(K, q, ε, method, ψ₀)
 
-#trueresult = [law(random, r) for r in R]
-
-
 res = Optim.minimizer(opt)
 
-
-#plot(lList, qOpti)
-#plot!(lList, q)
-
-plotOptiRes(rList, res, Ψ)
+plotOptiRes(rList, res, Ψ, ψ₀)
