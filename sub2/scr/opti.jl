80f2635ab25214cafe1e2ad71b3b450c77a19fe2
Marie Boussard <marie.boussard@student-cs.fr>
2023-09-13 14:05:17 +0200

    changing optimization script

--- sub2/scr/opti.jl
+++ sub2/scr/opti.jl
@@ -1,24 +1,27 @@
 using Distances
 using Optim
 
-function loss(K::Matrix, ψ::Vector, q::Vector, ε::Real)
+function loss(K::Matrix, ψ::Vector, q::Vector, ε::Real, Ψₘ::Real=0.2)
     @assert length(collect(axes(K)[2])) == length(ψ)
     @assert length(collect(axes(K)[1])) == length(q)
-    sqeuclidean(K*ψ,q)+ ε*norm(ψ.-.2)^2
+    sqeuclidean(K * ψ, q) + ε * norm(ψ .- Ψₘ)^2
 end
 
 defaultInit(ψ::Vector) = zero(ψ)
 
 
-function optimizePSD(K::Matrix, q::Vector, ε::Real, method = LBFGS(), ψ₀ = defaultInit(length(q)), loss = loss)
-    Optim.optimize(ψ->loss(K,ψ,q,ε), ψ₀;iterations = 100000)
+function optimizePSD(K::Matrix, q::Vector, ε::Real, method=LBFGS(), ψ₀=defaultInit(length(q)), loss=loss)
+    Optim.optimize(ψ -> loss(K, ψ, q, ε), ψ₀; iterations=100000)
 end
 
+function plotOptiRes(rList, Ψₙᵤₘ)
+    plot(rList, Ψₙᵤₘ)#, yticks=0:0.4)
+end
 
 function computeError(ψₙᵤₘ, ψₜᵣᵤₑ)
-    sqeuclidean(ψₙᵤₘ,ψₜᵣᵤₑ)
+    sqeuclidean(ψₙᵤₘ, ψₜᵣᵤₑ)
 end
 
 function plotError(ψₙᵤₘ, ψₜᵣᵤₑ)
-    
+
 end
--- sub2/scripts/script_opti.jl
+++ sub2/scripts/script_opti.jl
@@ -7,48 +7,70 @@ include("../scr/random.jl")
 
 
 # Inputs
-sizeR = 3
-sizeL = 3
+sizeR = 10
+sizeL = 1000
 ntir = 1000000
+ntirCLD = ntir
 
 # Random size
-random = Uniform(5,10)
-R = tir(random, sizeR)
+random = Uniform(5, 10)
+#random = Normal(5, 0.1)
+#R = tir(random, sizeR)
 
 # Given L (comes from the mesurement)
-L = range(1,4,sizeL) # Problème ??
+#L = range(1,4,sizeL) # Problème ??
 
 # Base Shape
 shape = Cube(1)
 
+# L comes is randomly chosen in the experimental cld obtained with the shape
+Ltir = computeCLD(shape, ntir)
+
 # Utils function
-bins, bins_number = computeCumulCLD(shape, ntir, 10000)
+#bins, bins_number = computeCumulCLD(shape, ntir, 10000)
 
 # Computation of the matrix K
-K = matrixCLD()
+rMax = 20
+lMax = maximum(Ltir)
+rList = collect(range(0, rMax, sizeR))
+lList = collect(range(0, lMax, sizeL))
+cumulCLD = computeCumulCLD(Ltir)
+K = matrixCLD(sizeL, sizeR, sorted(Ltir), rList, cumulCLD)
 
 # Computation of the CLD for different size
 function computeCLDdifferentSize()
     cld = zeros(ntir)
+    # Random measurement
     for i in 1:ntir
         r = tir(random, 1)[1]
         cp = Cube(r)
         cld[i] = computeCL(cp)
     end
-    bins,bins_number = computeCumulCLD(cld, 10000)
-    [probaCLD(l,1,bins,bins_number) for l in L]
+    bins, bins_number = computeCumulCLD(cld, 10000)
+    [probaCLD(l, cumulCLD) for l in range(0, lMax, sizeL)]
 end
 q = computeCLDdifferentSize()
 
+#function computeCLDdifferentSize(sizeR::Int, rList::Vector)
+#    q = zeros(sizeR)
+#    for r in rList
+#        cld = 
 
 # Optimisation
-ψ₀ = zeros(sizeR)
+ψ₀ = [0.2 for i in 1:sizeR]
 method = LBFGS()
 ε = 0.1
 
 opt = optimizePSD(K, q, ε, method, ψ₀)
 
-trueresult = [law(random,r) for r in R]
+#trueresult = [law(random, r) for r in R]
+
+trueresult = apply(x -> x >= 5 && x <= 10 ? 0.2 : 0, rList)
 
 res = Optim.minimizer(opt)
 
+#plotOptiRes(rList, res)
+
+qOpti = K * trueresult
+plot(lList, qOpti)
+plot!(lList, q)
