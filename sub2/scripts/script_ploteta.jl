989815fc45574cc9c8446e977720c99d316a1caa
Duez Theo <theozeud@gmail.com>
2023-09-18 10:21:23 +0200

    last changes

--- sub2/scripts/script_ploteta.jl
+++ sub2/scripts/script_ploteta.jl
@@ -2,18 +2,15 @@ include("../scr/shape.jl")
 include("../scr/cld.jl")
 include("../scr/plot.jl")
 
-#Inputs
-c = 4
 
-shape = Cube(c)
 
-cld = computeCLD(shape, 100000; Φ = 0, θ = 0)
+shape =  ConvexPolyhedron([(-2,0,0),(2,0,0),(-1,1,0),(1,1,0)])
 
-η₁ = c
-η₂ = c*√(2)
+cld = computeCLD(shape, 1000000; Φ = 0, θ = 0)
 
 plotCLD(cld)
 
+#=
 function theory(x)
     if x<c
         return 0
@@ -23,5 +20,5 @@ function theory(x)
 end
 
 plot!(range(0,6,1000),theory.(range(0,6,1000)))
-vline!([η₁,η₂])
 
+=#
--- sub2/scripts/script_rectangularcuboid.jl
+++ sub2/scripts/script_rectangularcuboid.jl
@@ -2,6 +2,7 @@ include("../scr/shape.jl")
 include("../scr/cld.jl")
 include("../scr/plot.jl")
 
+
 shape = RectanguarCuboid(2, 4, 6)
 
 @time cld = computeCLD(shape, 500000)
