dd6bf7c27aa7ad828f888ecee9908fc80ba7c013
Duez Theo <theo.duez@student-cs.fr>
2023-10-05 15:54:36 +0200

    rendu intermédiaire

--- sub/finalcode.jl
+++ sub/finalcode.jl
@@ -4,10 +4,10 @@ using LaTeXStrings
 using FFTW
 using Interpolations
 
-function dynamics(ψ₀fun= x->exp(-x^2), Vfun = (x,t) -> 0; L=10, Nₓ=1000, T=4, Nₜ=1000)
+function dynamics(ψ₀fun= x->1/sqrt(2π)*exp(-x^2), Vfun = (x,t) -> 0; L=20, Nₓ=1000, T=4, Nₜ=1000)
 
     # Space discretization
-    space = range(-L/2,L/2,Nₓ)[1:Nₓ]
+    space = range(-L/2,L/2,Nₓ+1)[1:Nₓ]
 
     # Initial condition
     ψ₀ = ψ₀fun.(space)
@@ -31,9 +31,6 @@ function dynamics(ψ₀fun= x->exp(-x^2), Vfun = (x,t) -> 0; L=10, Nₓ=1000, T=
     
     # Loop to compute the solution for each time step
     for i in 2:Nₜ+1
-        stepV[i] .* sol[:,i-1]
-        fft(stepV[i] .* sol[:,i-1])
-        stepΔ .* fft(stepV[i] .* sol[:,i-1])
         sol[:,i] = ifft(stepΔ .* fft(stepV[i] .* sol[:,i-1]))
     end
     sol
@@ -66,9 +63,9 @@ function animation(ψ::Matrix, duration::Real = 10, fps::Int = 30, L::Real = 10)
 
         interp_linear = [linear_interpolation(time, ψ[i,:])(t) for i in eachindex(ψ[:,1])]
         
-        pltreal = plot(space,real.(interp_linear), label = "Real", size=(700,400), xlim = [-L,L], ylim= [m,M])
-        pltimag = plot(space,imag.(interp_linear), label = "Imag", size=(700,400), xlim = [-L,L], ylims= [m,M])
-        pltabs  = plot(space, abs.(interp_linear), label = "Abs",  size=(700,400), xlim = [-L,L], y= [m,M])
+        pltreal = plot(space,real.(interp_linear), label = "Real", size=(700,400), xlim = [-L/2,L/2], ylim= [m,M])
+        pltimag = plot(space,imag.(interp_linear), label = "Imag", size=(700,400), xlim = [-L/2,L/2], ylims= [m,M])
+        pltabs  = plot(space, abs.(interp_linear), label = "Abs",  size=(700,400), xlim = [-L/2,L/2], y= [m,M])
 
         plot(pltreal, pltimag, pltabs, layout=(1,3), plot_title="Approximation by the Fourier Transform")
 
@@ -76,5 +73,32 @@ function animation(ψ::Matrix, duration::Real = 10, fps::Int = 30, L::Real = 10)
     gif(anim, fps = fps)
 end
 
+T = 10
+Nₜ = 10
+Nₓ = 100
+L = 10
+σ = 0.2
+duration = 50
+V = (x,t) -> norm(x)>L/4 ? 100 : 0
+ψ₀(x) = 1/(sqrt(2π)*σ)*exp(-x^2/(σ^2))
 
-@time animation(dynamics(Nₓ=100,T=10,Nₜ=100))
+#sol = dynamics(ψ₀, V, L = L, Nₓ=Nₓ,T=T,Nₜ=Nₜ)
+#t = @time animation(sol, duration)
+
+
+space = range(-L/2,L/2,Nₓ+1)[1:Nₓ]
+
+X = ψ₀.(space)
+
+@show norm(X)
+@show norm(fft(X))
+@show norm(ifft(fft(X)))
+
+function iter(X, n = 1000)
+    if n == 0
+        return []
+    end
+    c = ifft(exp.(-im.*fftfreq(length(X),length(X))).*fft(X))
+    return [norm(c), iter(c, n-1)...]
+end
+
